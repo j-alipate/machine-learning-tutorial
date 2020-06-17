5002cdb690081e5ee0a39829d6746eeddf047aa3
j-alipate <j.alipate@gmail.com>
2020-06-17 21:10:27 +1200

    added scatterplot

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ async function getData() {
     
     return cleaned;
   }
+
+
+  async function run() {
+    // Load and plot the original input data that we are going to train on.
+    const data = await getData();
+    const values = data.map(d => ({
+      x: d.horsepower,
+      y: d.mpg,
+    }));
+  
+    tfvis.render.scatterplot(
+      {name: 'Horsepower v MPG'},
+      {values}, 
+      {
+        xLabel: 'Horsepower',
+        yLabel: 'MPG',
+        height: 300
+      }
+    );
+  
+    // More code will be added below
+  }
+  
+  document.addEventListener('DOMContentLoaded', run);
